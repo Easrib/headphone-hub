b952308a182422167b69520887775ec405595678
Easrib <easrib.sharik01@gmail.com>
2022-03-25 23:41:36 +0600

    bootstrap installed

---
+++ src/Navabr/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>Headphone <span className='hub-text'>Hub</span></h1>
+            <p>Choose your Headphone</p>
+        </div>
+    );
+};
+
+export default Navbar;
